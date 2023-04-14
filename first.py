f127b24ea00616b1680b842a0dc2ee3b4d95165c
Krutik-patel <krutikmpatel@gmail.com>
2023-04-15 04:26:29 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.service import Service
+from fake_useragent import UserAgent
+import undetected_chromedriver as uc
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument(f"user-agent={UserAgent.random}")
+options.add_argument("user-data-dir=./")
+options.add_experimental_option("detach", True)
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+
+driver = uc.Chrome(chrome_options=options)
+
